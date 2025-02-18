d4e7fc938b497d636d752fc2f17c0aab274cc66f
VitorGulicz <vdmgulicz@gmail.com>
2025-02-18 20:26:51 -0300

    update

--- Exe01_VitorGulicz.py
+++ Exe01_VitorGulicz.py
@@ -1,2 +1,7 @@
 #Faça um programa que leia os numeros digitados pelo usuario, a cada numero digitado ele deve ser somado ao anterior e a cada soma deve ser exibida na tela. Quando a soma for 
 # superior a 50 ele deve exibir a mensagem "o total é :" e parar o programa 
+soma=0
+while soma<=50:
+    numero=int(input("Digite um numero: "))
+    soma+=numero
+print("O total é: ",soma, " -Vitor Gulicz")
