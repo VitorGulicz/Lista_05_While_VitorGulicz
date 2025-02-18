c576b931bd16a3a45f0a6353a6e1615a5d7a2446
VitorGulicz <vdmgulicz@gmail.com>
2025-02-18 20:30:22 -0300

    update

--- Exe02_VitorGulicz.py
+++ Exe02_VitorGulicz.py
@@ -1,6 +1,6 @@
 #Peça ao usuario para inserir um numero. Continue perguntando até que ele insira 5 numeros e em seguida exiba a mensagem "o ultimo numero que voce digitou foi: " e pare o programa
 i=0
-while i<=5:
+while i<=4:
     numero=int(input("Digite um numero: "))
     i+=1
 print("O ultimo numero digitado foi: ",numero," -Vitor Gulicz")
